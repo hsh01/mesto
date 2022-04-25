63be851a65030bd77b2626caf434002ef5b94dab
Shamil K <hsh0@ya.ru>
2022-04-25 18:47:18 +0300

    enhancement/remove_places

--- script/index.js
+++ script/index.js
@@ -1,45 +1,52 @@
 let editButton = document.querySelector('.profile__edit-button');
 let addButton = document.querySelector('.profile__add-button');
-let popup = document.querySelector('.popup');
-let popupAdd = document.querySelector('.popup_add_new-place');
+
+let popupEdit = document.querySelector('#popup__edit-profile');
+let popupAdd = document.querySelector('#popup__add-place');
+
 let title = document.querySelector('.profile__title');
 let subtitle = document.querySelector('.profile__subtitle');
-let closeButtons = document.querySelectorAll('.popup__close');
-let formElement = document.querySelector('.popup__form');
+
+let profileForm = document.querySelector('#popup__edit-profile .popup__form');
+let placeForm = document.querySelector('#popup__add-place .popup__form');
+
 let nameInput = document.querySelector('.popup__form-input[name=name]');
 let jobInput = document.querySelector('.popup__form-input[name=job]');
+
 let placeNameInput = document.querySelector('.popup__form-input_add_place-name[name=place-name]');
 let placeLinkInput = document.querySelector('.popup__form-input_add_place-link[name=place-link]');
 
+let closeButtons = document.querySelectorAll('.popup__close');
+
+function openPopup(popupElement) {
+    popupElement.classList.add('popup_opened');
+}
+
 function formEdit() {
-    popup.classList.add('popup_opened');
+    openPopup(popupEdit);
     nameInput.value = title.textContent;
     jobInput.value = subtitle.textContent;
 }
 
-function close() {
-    popup.classList.remove('popup_opened');
-}
-
-function formAddEdit() {
-    popupAdd.classList.add('popup_opened');
-    placeNameInput.value = title.textContent;
-    placeLinkInput.value = subtitle.textContent;
+function close(evt) {
+    evt.target.parentElement.parentElement.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     title.textContent = nameInput.value;
     subtitle.textContent = jobInput.value;
-    close();
+    close(evt);
 }
 
 editButton.addEventListener('click', formEdit);
-addButton.addEventListener('click', formAddEdit);
+addButton.addEventListener('click', function() {
+    openPopup(popupAdd);
+});
+profileForm.addEventListener('submit', formSubmitHandler);
 closeButtons.forEach(el => el.addEventListener('click', close));
-formElement.addEventListener('submit', formSubmitHandler);
 
-const initialCards = [{
+let initialCards = [{
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
@@ -73,20 +80,31 @@ function add_place(element) {
     const placeElement = placeTemplate.cloneNode(true);
     placeElement.querySelector('.place__title').textContent = element.name;
     placeElement.querySelector('.place__image').src = element.link;
-    places.append(placeElement);
+    places.prepend(placeElement);
 }
 
-initialCards.forEach(add_place);
+initialCards.reverse().forEach(add_place);
 
-function PlaceSubmitHandler(evt) {
+let removeButtons = document.querySelectorAll('.place__remove');
+
+function placeSubmitHandler(evt) {
     evt.preventDefault();
-    initialCards.unshift({
+    add_place({
         name: placeNameInput.value,
         link: placeLinkInput.value
     });
-    places.replaceChildren();
-    initialCards.forEach(add_place);
-    close();
+    close(evt);
 }
 
-formElement.addEventListener('submit', PlaceSubmitHandler);
+placeForm.addEventListener('submit', placeSubmitHandler);
+
+function remove_place(place) {
+    place.remove();
+    // initialCards = initialCards.filter(element => element.name !== place.querySelector('.place__title').textContent && element.link !== place.querySelector('.place__image').src);
+    // refresh_places();
+}
+
+removeButtons.forEach(el => el.addEventListener('click', function(evt) {
+    let place = evt.target.parentElement;
+    remove_place(place);
+}));
