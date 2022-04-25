a752f056a42506005ff05cee6cfa02ea20cba219
Shamil <hsh0@ya.ru>
2022-04-25 16:29:14 +0300

    addd

--- script/index.js
+++ script/index.js
@@ -1,11 +1,15 @@
 let editButton = document.querySelector('.profile__edit-button');
+let addButton = document.querySelector('.profile__add-button');
 let popup = document.querySelector('.popup');
+let popupAdd = document.querySelector('.popup_add_new-place');
 let title = document.querySelector('.profile__title');
 let subtitle = document.querySelector('.profile__subtitle');
 let closeButton = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__form-input[name=name]');
 let jobInput = document.querySelector('.popup__form-input[name=job]');
+let placeNameInput = document.querySelector('.popup__form-input_add_place-name[name=place-name]');
+let placeLinkInput = document.querySelector('.popup__form-input_add_place-link[name=place-link]');
 
 function formEdit() {
     popup.classList.add('popup_opened');
@@ -17,6 +21,16 @@ function close() {
     popup.classList.remove('popup_opened');
 }
 
+function formAddEdit() {
+  popupAdd.classList.add('popup_opened');
+  placeNameInput.value = title.textContent;
+  placeLinkInput.value = subtitle.textContent;
+}
+
+function close() {
+  popupAdd.classList.remove('popup_opened');
+}
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
     title.textContent = nameInput.value;
@@ -25,5 +39,33 @@ function formSubmitHandler(evt) {
 }
 
 editButton.addEventListener('click', formEdit);
+addButton.addEventListener('click', formAddEdit);
 closeButton.addEventListener('click', close);
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
