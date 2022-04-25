e8efbc610e99d3c876422e9f1eae831051c67b12
Shamil K <hsh0@ya.ru>
2022-04-25 21:09:58 +0300

    add fullscreen

--- script/index.js
+++ script/index.js
@@ -3,6 +3,7 @@ let addButton = document.querySelector('.profile__add-button');
 
 let popupEdit = document.querySelector('#popup__edit-profile');
 let popupAdd = document.querySelector('#popup__add-place');
+let popupFullscreen = document.querySelector('#popup__place-fullscreen');
 
 let title = document.querySelector('.profile__title');
 let subtitle = document.querySelector('.profile__subtitle');
@@ -78,16 +79,34 @@ const places = document.querySelector('.places');
 const placeTemplate = document.querySelector('.place-template').content;
 
 
+function remove_place_listener(el) {
+    el.addEventListener('click', function(evt) {
+        evt.target.parentElement.remove();
+    });
+}
+
+function place_fullscreen(evt) {
+    openPopup(popupFullscreen);
+    const fullscreenImage = popupFullscreen.querySelector('.popup__img');
+    fullscreenImage.src = evt.target.src;
+    fullscreenImage.alt = evt.target.alt;
+    popupFullscreen.querySelector('.popup__img-caption').textContent = evt.target.alt
+}
+
+
 function add_place(element) {
     const placeElement = placeTemplate.cloneNode(true);
     placeElement.querySelector('.place__title').textContent = element.name;
     placeElement.querySelector('.place__image').src = element.link;
+    placeElement.querySelector('.place__image').alt = element.name;
+    const remove_button = placeElement.querySelector('.place__remove');
+    remove_button.addEventListener('click', (evt) => evt.target.parentElement.remove());
+    placeElement.querySelector('.place__image').addEventListener('click', (evt) => place_fullscreen(evt));
     places.prepend(placeElement);
 }
 
 initialCards.reverse().forEach(add_place);
 
-let removeButtons = document.querySelectorAll('.place__remove');
 
 function placeSubmitHandler(evt) {
     evt.preventDefault();
@@ -99,14 +118,3 @@ function placeSubmitHandler(evt) {
 }
 
 placeForm.addEventListener('submit', placeSubmitHandler);
-
-function remove_place(place) {
-    place.remove();
-    // initialCards = initialCards.filter(element => element.name !== place.querySelector('.place__title').textContent && element.link !== place.querySelector('.place__image').src);
-    // refresh_places();
-}
-
-removeButtons.forEach(el => el.addEventListener('click', function(evt) {
-    let place = evt.target.parentElement;
-    remove_place(place);
-}));
