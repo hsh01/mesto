258f2ace20d6690c318464ccee21448778191694
Shamil K <hsh0@ya.ru>
2022-06-04 22:16:10 +0500

    api class view

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,78 +1,98 @@
-const base_url = `https://nomoreparties.co/v1/`;
+class Api {
+    constructor({baseUrl, headers},
+                errorCallback = (err) => console.log(err)) {
+        this._baseUrl = baseUrl;
+        this._headers = headers;
+        this._errorCallback = errorCallback;
+    }
 
-async function _request(input: RequestInfo,
-                        init?: RequestInit,
-                        errorCallback = (err) => alert(err),
-                        loadingCallback) {
-    loadingCallback(true);
-
-    init.headers.append('authorization', 'c56e30dc-2883-4270-a59e-b2f7bae969c6');
-    init.headers.append('Content-Type', 'application/json');
-
-    return await fetch(input, init)
-        .then(res => {
-            if (res.ok) return res.json();
-            return Promise.reject(res.statusText);
-        })
-        .catch(err => errorCallback(err))
-        .finally(loadingCallback(false));
+    _request(path,
+             init = undefined,
+             loadingCallback = undefined) {
+        if (loadingCallback) {
+            loadingCallback(true);
+        }
+        init.headers = {...init.headers, ...this._headers};
+        const result = null;
+        return fetch(`${this._baseUrl}/${path}`, init)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+                return Promise.reject(res.status);
+            })
+            .catch(err => this._errorCallback(err))
+            .finally(() => {
+                if (loadingCallback) loadingCallback(false);
+            });
+    }
 }
 
-async function getUserInfo(cohortId, errorCallback, loadingCallback) {
-    const url = `${base_url}${cohortId}/users/me`;
-    return await _request(url, {}, errorCallback, loadingCallback);
+class UserApi extends Api {
+    constructor(options, errorCallback) {
+        super(options, errorCallback);
+    }
+
+    getUserInfo(loadingCallback = undefined) {
+        const path = 'users/me';
+        return this._request(path, {}, loadingCallback);
+    }
+
+    patchUserInfo(data, loadingCallback = undefined) {
+        const path = 'users/me';
+        return this._request(path, {
+            method: 'PATCH',
+            body: JSON.stringify(data)
+        }, loadingCallback);
+    }
+
+    patchUserAvatar(data, loadingCallback = undefined) {
+        const path = `users/me/avatar`;
+        return this._request(path, {
+            method: 'PATCH',
+            body: JSON.stringify(data)
+        }, loadingCallback);
+    }
 }
 
-async function patchUserInfo(data, cohortId, errorCallback, loadingCallback) {
-    const url = `${base_url}${cohortId}/users/me`;
-    return await _request(url, {
-        method: 'PATCH',
-        body: JSON.stringify(data)
-    }, errorCallback, loadingCallback);
+class CardApi extends Api {
+    constructor(options, errorCallback) {
+        super(options, errorCallback);
+    }
+
+    getCards(loadingCallback = undefined) {
+        const path = 'cards';
+        return this._request(path, {}, loadingCallback);
+    }
+
+    postCard(data, loadingCallback = undefined) {
+        const path = 'cards';
+        return this._request(path, {
+            method: 'POST',
+            body: JSON.stringify(data)
+        }, loadingCallback);
+    }
+
+    deleteCard(cardId, loadingCallback = undefined) {
+        const path = `cards/${cardId}`;
+        return this._request(path, {
+            method: 'DELETE',
+        }, loadingCallback);
+    }
+
+    putLike(cardId, loadingCallback = undefined) {
+        const path = `cards/${cardId}/likes`;
+        return this._request(path, {
+            method: 'PUT',
+        }, loadingCallback);
+    }
+
+    deleteLike(cardId, loadingCallback = undefined) {
+        const path = `cards/${cardId}/likes`;
+        return this._request(path, {
+            method: 'DELETE',
+        }, loadingCallback);
+    }
 }
 
-async function patchUserAvatar(data, cohortId, errorCallback, loadingCallback) {
-    const url = `${base_url}${cohortId}/users/me/avatar`;
-    return await _request(url, {
-        method: 'PATCH',
-        body: JSON.stringify(data)
-    }, errorCallback, loadingCallback);
-}
-
-async function getCards(cohortId, errorCallback, loadingCallback) {
-    const url = `${base_url}${cohortId}/cards`;
-    return await _request(url, {}, errorCallback, loadingCallback);
-}
-
-async function postCard(data, cohortId, errorCallback, loadingCallback) {
-    const url = `${base_url}${cohortId}/cards`;
-    return await _request(url, {
-        method: 'POST',
-        body: JSON.stringify(data)
-    }, errorCallback, loadingCallback);
-}
-
-
-async function deleteCard(cardId, cohortId, errorCallback, loadingCallback) {
-    const url = `${base_url}${cohortId}/cards/${cardId}`;
-    return await _request(url, {
-        method: 'DELETE',
-    }, errorCallback, loadingCallback);
-}
-
-async function putLike(cardId, cohortId, errorCallback, loadingCallback) {
-    const url = `${base_url}${cohortId}/cards/${cardId}/likes`;
-    return await _request(url, {
-        method: 'PUT',
-    }, errorCallback, loadingCallback);
-}
-
-async function deleteLike(cardId, cohortId, errorCallback, loadingCallback) {
-    const url = `${base_url}${cohortId}/cards/${cardId}/likes`;
-    return await _request(url, {
-        method: 'DELETE',
-    }, errorCallback, loadingCallback);
-}
-
-
-export {getUserInfo}
+export {Api, UserApi, CardApi}
