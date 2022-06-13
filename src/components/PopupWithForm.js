048ddeed9ffb311523018e4fafc6ce678f661d0e
Shamil K <hsh0@ya.ru>
2022-06-13 21:54:04 +0500

    refactor

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,12 +6,35 @@ class PopupWithForm extends Popup {
     constructor(formSelector, callbackSubmitForm) {
         super(formSelector);
         this._form = this._element.querySelector('.form');
-        this._inputs = this._form.querySelectorAll('.form__input')
+        this._inputs = this._form.querySelectorAll('.form__input');
+        this._button = this._form.querySelector('.form__submit');
         this._callbackSubmitForm = callbackSubmitForm;
     }
 
-    getSubmitButton() {
-        return this._form.querySelector('.form__submit');
+    popupLoaderCallback(loading, loadText = 'Сохранение') {
+        if (loading) {
+            this._button.classList.add('form__submit_loading');
+            this._button.textContent = loadText;
+        } else {
+            this._button.classList.remove('form__submit_loading');
+        }
+    }
+
+    popupSuccessCallback(initialText = 'Сохранить', loadedText = 'Сохранено') {
+        this._button.classList.add('form__submit_loading-ok');
+        this._button.textContent = loadedText;
+        setTimeout(() => {
+            this._button.classList.remove('form__submit_loading-ok');
+            this._button.textContent = initialText;
+            this.close();
+        }, 1000);
+    }
+
+    popupErrorCallback(err, initialText = 'Сохранить') {
+        this._button.textContent = `Ошибка: ${err}.`;
+        setTimeout(() => {
+            this._button.textContent = initialText;
+        }, 3000);
     }
 
     close() {
